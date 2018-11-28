69ebc99bfedb65653143e7f3b13c1f350b6acfdd
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2018-11-28 15:47:00 -0600

    Found a bug in my class method post

--- app/views.py
+++ app/views.py
@@ -5,20 +5,19 @@ from django.views import View
 
 class Create(View):
     def post(self, request):
-        if request.method == "POST":
-            url = request.POST["url"]
-            link = Link.shorten(url)
-            if link is None:
-                return render(
-                    request,
-                    "app/create.html", {"invalid_url": True},
-                    status=422)
-            else:
-                return redirect('app:show', link.short_code)
-        return render(
-            request,
-            'app/create.html',
-        )
+        url = request.POST["url"]
+        link = Link.shorten(url)
+        if link is None:
+            return render(
+                request, "app/create.html", {"invalid_url": True}, status=422)
+        else:
+            return redirect('app:show', link.short_code)
+
+        def get(self, request):
+            return render(
+                request,
+                'app/create.html',
+            )
 
 
 class Show(View):
