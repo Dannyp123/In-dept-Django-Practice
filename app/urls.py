df0cfec8cd8eb9d2d81d838bfdc14c19387766df
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2018-11-28 15:38:22 -0600

    Turned all of my function based views into class based views

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'app'
 
 urlpatterns = [
-    path('', views.creating_view, name='create'),
-    path('link/<short_code>/', views.showing, name='show'),
-    path('<short_code>/', views.go_to, name='goto')
+    path('', views.Create.as_view(), name='create'),
+    path('link/<short_code>/', views.Show.as_view(), name='show'),
+    path('<short_code>/', views.Goto.as_view(), name='goto')
 ]
--- app/views.py
+++ app/views.py
@@ -1,30 +1,36 @@
 from django.shortcuts import render, redirect
 from app.models import Link
+from django.views import View
 
 
-def creating_view(request):
-    if request.method == "POST":
-        url = request.POST["url"]
-        link = Link.shorten(url)
-        if link is None:
-            return render(
-                request, "app/create.html", {"invalid_url": True}, status=422)
+class Create(View):
+    def post(self, request):
+        if request.method == "POST":
+            url = request.POST["url"]
+            link = Link.shorten(url)
+            if link is None:
+                return render(
+                    request,
+                    "app/create.html", {"invalid_url": True},
+                    status=422)
+            else:
+                return redirect('app:show', link.short_code)
+        return render(
+            request,
+            'app/create.html',
+        )
+
+
+class Show(View):
+    def get(self, request, short_code):
+        return render(request, "app/show.html",
+                      {'link': Link.find_by_short_code(short_code)})
+
+
+class Goto(View):
+    def get(self, request, short_code):
+        page = Link.find_by_short_code(short_code)
+        if page is None:
+            return redirect("app:create")
         else:
-            return redirect('app:show', link.short_code)
-    return render(
-        request,
-        'app/create.html',
-    )
-
-
-def showing(request, short_code):
-    return render(request, "app/show.html",
-                  {'link': Link.find_by_short_code(short_code)})
-
-
-def go_to(request, short_code):
-    page = Link.find_by_short_code(short_code)
-    if page is None:
-        return redirect("app:create")
-    else:
-        return redirect(page.original)
+            return redirect(page.original)
