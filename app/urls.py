b4ac328d80ab6f8df6183e226b3fdbb4bf5982d8
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2018-11-28 14:48:51 -0600

    first inital commit

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from . import views
 
 app_name = 'app'
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.creating_view, name='create'),
+    path('link/<short_code>/', views.showing, name='show'),
+    path('<short_code>/', views.go_to, name='goto')
+]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from app.models import Link
 
-# Create your views here.
+
+def creating_view(request):
+    if request.method == "POST":
+        url = request.POST["url"]
+        link = Link.shorten(url)
+        if link is None:
+            return render(
+                request, "app/create.html", {"invalid_url": True}, status=422)
+        else:
+            return redirect('app:show', link.short_code)
+    return render(
+        request,
+        'app/create.html',
+    )
+
+
+def showing(request, short_code):
+    return render(request, "app/show.html",
+                  {'link': Link.find_by_short_code(short_code)})
+
+
+def go_to(request, short_code):
+    page = Link.find_by_short_code(short_code)
+    if page is None:
+        return redirect("app:create")
+    else:
+        return redirect(page.original)
